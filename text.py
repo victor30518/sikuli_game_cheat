ceffc4fc155b9bb69c247b8b63323389a420d47c
victor30518 <r06946009@ntu.edu.tw>
2023-03-09 23:29:59 +0800

    Upload the images and code

---
+++ text.py
@@ -0,0 +1,41 @@
+click("1483217302234.png")
+wait(2)
+click("1483217395811.png")
+region = Region(1072,38,545,999)
+mouse = region.findAll("1483218522630.png")
+cattle = region.findAll("1483218544003.png")
+tiger = region.findAll("1483218553959.png")
+rabbit = region.findAll("1483218567068.png")
+dragon = region.findAll("1483217936393.png")
+snake = region.findAll("1483218582225.png")
+horse = region.findAll("1483218227171.png")
+sheep = region.findAll("1483218591034.png")
+monkey = region.findAll("1483218610958.png")
+chicken = region.findAll("1483218620497.png")
+dog = region.findAll("1483218629230.png")
+pig = region.findAll("1483218641288.png")
+wait(5)
+for x in mouse:
+    click(x)
+for x in cattle:
+    click(x)
+for x in tiger:
+    click(x)
+for x in rabbit:
+    click(x)
+for x in dragon:
+    click(x)
+for x in snake:
+    click(x)
+for x in horse:
+    click(x)
+for x in sheep:
+    click(x)
+for x in monkey:
+    click(x)
+for x in chicken:
+    click(x)
+for x in dog:
+    click(x)
+for x in pig:
+    click(x)
